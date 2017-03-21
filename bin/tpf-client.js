bbd7775c422a0278d5277ef9372daadc6f15a065
Thomas Minier <thomas.minier@protonmail.com>
2017-03-21 11:30:18 +0100

    add an option for mime-type in cli

--- bin/tpf-client.js
+++ bin/tpf-client.js
@@ -38,6 +38,7 @@ program
   .usage('[options] [endpoints...]')
   .option('-q, --query <query>', 'evaluates the given SPARQL query')
   .option('-f, --file <file>', 'evaluates the SPARQL query in the given file')
+  .option('-t, --type <mime-type>', 'determines the MIME type of the output (e.g., application/json)', 'application/json')
   .parse(process.argv);
 
 // fetch SPARQL query to execute
@@ -45,20 +46,20 @@ let query = null;
 if (program.query) {
   query = program.query;
 } else if (program.file && fs.existsSync(program.file)) {
-  query = fs.readFileSync(program.file, 'utf8');
+  query = fs.readFileSync(program.file, 'utf-8');
 } else {
   process.stderr.write('Error: you must specify a SPARQL query to execute.\nSee ./tpf-client --help for more details.\n');
   process.exit(1);
 }
 
-// check endpoints
+// check number of endpoints
 if (program.args.length < 1) {
   process.stderr.write('Error: you must specify at least one endpoint to execute the query.\nSee ./tpf-client --help for more details.\n');
   process.exit(1);
 }
 
 const sparqlIterator = queryEngine(query, program.args);
-const writer = ldf.SparqlResultWriter.instantiate('application/json', sparqlIterator);
+const writer = ldf.SparqlResultWriter.instantiate(program.type, sparqlIterator);
 writer.on('data', data => process.stdout.write(data));
 writer.on('error', error => {
   process.stderr.write('ERROR: An error occurred during query execution.\n');
