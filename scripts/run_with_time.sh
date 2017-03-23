42c7944271b6650890acf355a567fe1289209d99
Thomas Minier <thomas.minier@protonmail.com>
2017-03-23 15:10:49 +0100

    increase js heap memory for experiments

--- scripts/run_with_time.sh
+++ scripts/run_with_time.sh
@@ -2,5 +2,5 @@
 # Run the tpf client with the time in seconds printed in stderr
 
 FILE=$1
-SERVERS='http://localhost:5000/dbpedia_3_8 http://fragments.mementodepot.org/dbpedia_3_8'
-/usr/bin/time -f %e -o execution_times.csv -a node bin/tpf-client.js -f $FILE $SERVERS > log 2>> err
+SERVERS='http://localhost:5000/dbpedia_3_8 http://localhost:5000/dbpedia_3_8'
+/usr/bin/time -f %e -o execution_times.csv -a node --max-old-space-size=8000 bin/tpf-client.js -f $FILE $SERVERS > log 2>> err
