784c20d6b9392e63bfb11ab737f131cd0c6adfe4
Thomas Minier <thomas.minier@protonmail.com>
2017-03-23 15:30:05 +0100

    save results & errors when executing a workload

--- scripts/run_with_time.sh
+++ scripts/run_with_time.sh
@@ -2,5 +2,6 @@
 # Run the tpf client with the time in seconds printed in stderr
 
 FILE=$1
+RESULTS=`basename $FILE`
 SERVERS='http://localhost:5000/dbpedia_3_8 http://localhost:5000/dbpedia_3_8'
-/usr/bin/time -f %e -o execution_times.csv -a node --max-old-space-size=8000 bin/tpf-client.js -f $FILE $SERVERS > log 2>> err
+/usr/bin/time -f %e -o execution_times.csv -a node --max-old-space-size=8000 bin/tpf-client.js -f $FILE $SERVERS > results/$RESULTS 2> errors/$RESULTS
--- scripts/run_workload.sh
+++ scripts/run_workload.sh
@@ -2,6 +2,9 @@
 # Run the tpf client with a workload of queries
 
 WORKLOAD=$*
+rm -f execution_times
+mkdir -p results/
+mkdir -p errors/
 touch execution_times.csv
 for f in $WORKLOAD; do
   ./scripts/run_with_time.sh $f
