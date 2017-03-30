6e61f734f99d9a9c6a83dc496e8bd2284b73d001
Thomas Minier <thomas.minier@protonmail.com>
2017-03-30 16:12:05 +0200

    update scripts + add results for reference exec. on amazon

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -11,16 +11,17 @@ function colors()
  return Scale.color_discrete_manual(colorant"#990000", colorant"#ff4000", colorant"#ffbf00")
 end
 
-ref = readtable("./execution_times_ref.csv")
-data = readtable("./execution_times.csv")
+ref = readtable("amazon/execution_times_ref.csv")
+data = readtable("amazon/execution_times.csv")
 
-names!(ref, [ :time ])
-names!(data, [ :time ])
 ref[:servers] = 1
 data[:servers] = 2
 
 all = [ref;data]
+big = all[all[:time] .>= 3.0, :]
 
-p = plot(all, x=:servers, y=:time,  color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, colors())
+p = plot(all, x=:servers, y=:time,  color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, Scale.y_log10, colors())
+pbig = plot(big, x=:servers, y=:time,  color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, Scale.y_log10, colors())
 
-draw(PDF("execution_time.pdf", 7inch, 5inch), p)
+draw(PDF("amazon/execution_time.pdf", 7inch, 5inch), p)
+draw(PDF("amazon/execution_time_greater_3s.pdf", 7inch, 5inch), pbig)
