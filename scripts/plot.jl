365af8da45a7442c7cba2fd5c5b404c145469125
Thomas Minier <thomas.minier@protonmail.com>
2017-03-31 12:40:52 +0200

    classify results per run

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -1,4 +1,5 @@
 using Gadfly
+using Dataframe
 using RDatasets
 
 panel_theme = Theme(
@@ -7,21 +8,45 @@ panel_theme = Theme(
 
 Gadfly.push_theme(panel_theme)
 
+# Custom color scale for plots
 function colors()
  return Scale.color_discrete_manual(colorant"#990000", colorant"#ff4000", colorant"#ffbf00")
 end
 
-ref = readtable("amazon/execution_times_ref.csv")
-data = readtable("amazon/execution_times.csv")
+# Concat results from three distinct runs
+function concatRuns(run1, run2, run3)
+  return hcat(run1, run2, run3)
+end
+
+# Compute the mean of three runs
+function meanRun(runs)
+  res = DataFrame(time = [])
+  for row in eachrow(runs)
+    push!(res, [ mean(convert(Array, row)) ])
+  end
+  return res
+end
+
+ref_run1 = readtable("amazon/run1/execution_times_ref.csv")
+ref_run2 = readtable("amazon/run2/execution_times_ref.csv")
+ref_run3 = readtable("amazon/run3/execution_times_ref.csv")
+data_run1 = readtable("amazon/run1/execution_times.csv")
+data_run2 = readtable("amazon/run2/execution_times.csv")
+data_run3 = readtable("amazon/run3/execution_times.csv")
+
+ref = meanRun(concatRuns(ref_run1, ref_run2, ref_run3))
+data = meanRun(concatRuns(data_run1, data_run2, data_run3))
 
 ref[:servers] = 1
+ref[:l] = 1
 data[:servers] = 2
+data[:l] = 1
 
 all = [ref;data]
 big = all[all[:time] .>= 3.0, :]
 
-p = plot(all, x=:servers, y=:time,  color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, Scale.y_log10, colors())
-pbig = plot(big, x=:servers, y=:time,  color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, Scale.y_log10, colors())
+p = plot(all, x=:servers, y=:time, color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, Scale.y_log10, colors())
+pbig = plot(big, x=:servers, y=:time, color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, Scale.y_log10, colors())
 
 draw(PDF("amazon/execution_time.pdf", 7inch, 5inch), p)
 draw(PDF("amazon/execution_time_greater_3s.pdf", 7inch, 5inch), pbig)
--- scripts/run_with_time.sh
+++ scripts/run_with_time.sh
@@ -10,6 +10,6 @@ if [ "$#" -ne 1 ]; then
 fi
 
 RESULTS=`basename $FILE`
-SERVERS='http://localhost:5000/dbpedia_3_8 http://localhost:5000/dbpedia_3_8'
+SERVERS='http://52.39.116.115/watDiv_100 http://52.33.245.25/watDiv_100'
 /usr/bin/time -f %e -o execution_times.csv -a bin/tpf-client.js run model.json $SERVERS -f $FILE -t application/sparql-results+xml > results/$RESULTS 2> errors/$RESULTS
-# /usr/bin/time -f %e -o execution_times.csv -a ldf-client http://localhost:5000/dbpedia_3_8 -f $FILE -t application/sparql-results+xml > results/$RESULTS 2> errors/$RESULTS
+# /usr/bin/time -f %e -o execution_times.csv -a ldf-client http://52.39.116.115/watDiv_100 -f $FILE -t application/sparql-results+xml > results/$RESULTS 2> errors/$RESULTS
