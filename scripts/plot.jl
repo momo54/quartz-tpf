1f6ff98361e07abaf2b585037e4aa786ac263a5d
Thomas Minier <thomas.minier@protonmail.com>
2017-04-24 16:51:05 +0200

    add first results for run with non equ. servers

--- scripts/plot.jl
+++ scripts/plot.jl
@@ -39,14 +39,18 @@ time_ref_run3 = readtable("amazon/run3/execution_times_ref.csv")
 time_data_run1 = readtable("amazon/run1/execution_times.csv")
 time_data_run2 = readtable("amazon/run2/execution_times.csv")
 time_data_run3 = readtable("amazon/run3/execution_times.csv")
+time_london_run1 = readtable("amazon/run1/execution_times_london.csv")
 
 time_ref = meanRun(concatRuns(time_ref_run1, time_ref_run2, time_ref_run3))
 time_data = meanRun(concatRuns(time_data_run1, time_data_run2, time_data_run3))
+time_london = meanRun(time_london_run1)
 
 time_ref[:query] = 1:nrow(time_ref_run1)
 time_ref[:servers] = "TPF-1"
 time_data[:query] = 1:nrow(time_data_run1)
-time_data[:servers] = "TPF+QR-VTP"
+time_data[:servers] = "TPF+QR-VTP-EQ"
+time_london[:query] = 1:nrow(time_london_run1)
+time_london[:servers] = "TPF+QR-VTP-NEQ"
 
 # Completeness
 compl_ref_run1 = readtable("amazon/run1/completeness_ref.csv")
@@ -62,10 +66,10 @@ compl_data = meanRun(concatRuns(compl_data_run1, compl_data_run2, compl_data_run
 compl_ref[:query] = 1:nrow(compl_ref_run1)
 compl_ref[:servers] = "TPF-1"
 compl_data[:query] = 1:nrow(compl_data_run1)
-compl_data[:servers] = "TPF+QR-VTP"
+compl_data[:servers] = "TPF+QR-VTP-EQ"
 
 # Gather dataframes for plots
-time_all = [time_ref;time_data]
+time_all = [time_ref;time_data;time_london]
 compl_all = [compl_ref;compl_data]
 # big = all[all[:time] .>= 1.0, :]
 
@@ -74,6 +78,6 @@ time_plot = plot(time_all, x=:query, y=:mean_value, color=:servers, Geom.bar(pos
 compl_plot = plot(compl_all, xgroup=:servers, x=:query, y=:mean_value, color=:servers, Geom.subplot_grid(Geom.point), Guide.xlabel("Queries"), Guide.ylabel("Answer completeness"), Scale.x_continuous, colors(), no_colors_guide)
 # pbig = plot(big, x=:servers, y=:time, color=:servers, Geom.boxplot, Guide.xlabel("Number of servers"), Guide.ylabel("Execution time (s)"), Scale.x_discrete, Scale.y_log10, colors())
 
-draw(PDF("amazon/execution_time.pdf", 7inch, 5inch), time_plot)
+draw(PDF("amazon/execution_time.pdf", 8inch, 5inch), time_plot)
 draw(PDF("amazon/completeness.pdf", 10inch, 3inch), compl_plot)
 # draw(PDF("amazon/execution_time_greater_3s.pdf", 7inch, 5inch), pbig)
