0497965f9b48cb4dfba96a9110414cfde8942271
Thomas Minier <thomas.minier@protonmail.com>
2017-05-11 09:25:09 +0200

    add models & scripts for next exp. with 4 servers

--- scripts/compile_models.sh
+++ scripts/compile_models.sh
@@ -2,7 +2,7 @@
 # compile all models for a set of queries stored in a file
 
 QUERIES=$1
-SERVERS='http://52.39.116.115/watDiv_100 http://52.33.245.25/watDiv_100 http://54.70.48.92/watDiv_100'
+SERVERS='http://52.39.116.115/watDiv_100 http://52.33.245.25/watDiv_100 http://54.70.48.92/watDiv_100 http://35.160.40.16/watDiv_100'
 # SERVERS='http://52.39.116.115/watDiv_100 http://35.177.243.45/watDiv_100'
 # SERVERS='http://localhost:5000/watDiv_100 http://localhost:5001/watDiv_100 http://localhost:5002/watDiv_100 http://localhost:5003/watDiv_100'
 cpt=1
@@ -19,7 +19,7 @@ mkdir -p models/
 while IFS='' read -r line || [[ -n "$line" ]]; do
   QFILE="queries/query$cpt"
   echo $line > $QFILE
-  bin/tpf-client.js model $SERVERS -f $QFILE -o models-3/query$cpt.json
+  bin/tpf-client.js model $SERVERS -f $QFILE -o models-4/query$cpt.json
   rm -f $QFILE
   cpt=$((cpt+1))
 done < "$QUERIES"
